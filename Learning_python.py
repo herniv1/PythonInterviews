d9272b0de10161f0beec0fbee5fd3bf5ae4b35a7
herniv1 <50881030+herniv1@users.noreply.github.com>
2025-03-03 21:59:28 -0600

    Adding list methods for Data structures

---
+++ Learning_python.py
@@ -0,0 +1,19 @@
+# swap two variables
+a = 5 
+b = 10
+a, b = 3,6
+print(a)
+print(b)
+
+# Data structure
+###############################
+
+list_a = [2, 5, 8, 12, 5]
+list_a.append(19)
+list_a.insert(0,44)
+list_a.remove(8)
+print(list_a)
+list_a.pop(2)
+print(list_a)
+c = list_a.index(5)
+print(c)
